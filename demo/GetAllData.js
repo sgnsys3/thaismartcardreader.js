d0446881bcdcb97d96ac1eae592f3ef3383f8c02
Pureewat Kaewpoy <pureewat.k@gmail.com>
2018-05-29 03:41:46 +0700

    [UPDATE] New demo

--- demo/GetAllData.js
+++ demo/GetAllData.js
@@ -1,8 +1,14 @@
 const fs = require('fs')
-const { Reader } = require('../src')
+const {
+  Reader
+} = require('../src')
 const path = require('path')
 
 const myReader = new Reader()
+
+process.on('unhandledRejection', (reason) => {
+  console.log('From Global Rejection -> Reason: ' + reason);
+});
 
 console.log('Waiting For Device !')
 myReader.on('device-activated', async (event) => {
@@ -15,21 +21,40 @@ myReader.on('error', async (err) => {
   console.log(err)
 })
 
-myReader.on('card-readed', (person) => {
-  console.log(`CitizenID: ${person.cid}`)  
-  console.log(`ThaiName: ${person.name.th}`)
-  console.log(`EnglishName: ${person.name.en}`)
-  console.log(`DOB: ${person.dob.day}/${person.dob.month}/${person.dob.year}`)
-  console.log(`Address: ${person.address}`)
-  console.log(`IssueDate: ${person.issueDate.day}/${person.issueDate.month}/${person.issueDate.year}`)
-  console.log(`Issuer: ${person.issuer}`)
-  console.log(`ExpireDate: ${person.expireDate.day}/${person.expireDate.month}/${person.expireDate.year}`)
-  console.log(`Image Saved to ${path.resolve('')}/${person.cid}.bmp`)
+myReader.on('image-reading', (percent) => {
+  console.log(percent)
+})
+
+myReader.on('card-inserted', async (person) => {
+  const cid = await person.getCid()
+  const thName = await person.getNameTH()
+  const enName = await person.getNameEN()
+  const dob = await person.getDoB()
+  const issueDate = await person.getIssueDate()
+  const expireDate = await person.getExpireDate()
+  const address = await person.getAddress()
+  const issuer = await person.getIssuer()
+
+  console.log(`CitizenID: ${cid}`)
+  console.log(`THName: ${thName.prefix} ${thName.firstname} ${thName.lastname}`)
+  console.log(`ENName: ${enName.prefix} ${enName.firstname} ${enName.lastname}`)
+  console.log(`DOB: ${dob.day}/${dob.month}/${dob.year}`)
+  console.log(`Address: ${address}`)
+  console.log(`IssueDate: ${issueDate.day}/${issueDate.month}/${issueDate.year}`)
+  console.log(`Issuer: ${issuer}`)
+  console.log(`ExpireDate: ${expireDate.day}/${expireDate.month}/${expireDate.year}`)
+
   console.log('=============================================')
-  const fileStream = fs.createWriteStream(`${person.cid}.bmp`)
-  const photoBuff = Buffer.from(person.photo)
+  console.log('Receiving Image')
+  const photo = await person.getPhoto()
+  console.log(`Image Saved to ${path.resolve('')}/${cid}.bmp`)
+  console.log('=============================================')
+  const fileStream = fs.createWriteStream(`${cid}.bmp`)
+  const photoBuff = Buffer.from(photo)
   fileStream.write(photoBuff)
   fileStream.close()
 })
 
-myReader.on('device-deactivated', () => { console.log('device-deactivated') })
+myReader.on('device-deactivated', () => {
+  console.log('device-deactivated')
+})
